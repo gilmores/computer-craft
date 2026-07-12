2c38e6a047368357ae2b8d0b77f248568e35636b
gilmores <spennycgilmore@gmail.com>
2026-07-11 22:25:41 -0600

    Add zigZagToLocation v1

--- blockFiller.lua
+++ blockFiller.lua
@@ -1,14 +1,51 @@
 local x, y, z
- 
-while true do
+local startX, startY, startZ = gps.locate()
+local endX, endY, endZ = 21, 77, -36
+local targetX, targetY, targetZ = endX, endY, endZ
+local running = true
+
+local forwardX = true
+local leftTurn = true
+
+local function digForward()
+    while turtle.detect() do
+        turtle.dig()
+        sleep(0.5)
+    end
+    turtle.forward()
+end
+
+local function zigZagToLocation(targetX, targetY, targetZ)
+    if x == targetX and y == targetY and z == targetZ then
+        running = false
+    end
+    if x == targetX then
+        -- Turn, move Z forward 1, turn, continue on x
+        if leftTurn then
+            turtle.turnLeft()
+            digForward()
+            turtle.turnLeft()
+            leftTurn = false
+        else
+            turtle.turnRight()
+            digForward()
+            turtle.turnRight()
+            leftTurn = true
+        end
+        if targetX == endX then
+            targetX = startX
+        else
+            targetX = endX
+        end
+    else
+        digForward()
+    end
+end
+
+while running do
     x, y, z = gps.locate()
-    print("x: "..x)
-    print("y: "..y)
-    print("z: "..z)
-    sleep(1)
+
+    zigZagToLocation(targetX, targetY, targetZ)
+
+    sleep(0.5)
 end
- 
-local function moveToLocation()
- 
-end
- 
